f1f03f071513d90696034176d1a21f2dfc95d86e
Ro Erb <35536460+rerb13@users.noreply.github.com>
2022-11-11 21:04:40 -0700

    Updated file commenting

--- WebScraper/Form1.cs
+++ WebScraper/Form1.cs
@@ -1,14 +1,7 @@
 ﻿/**
- * Author:    Ro Erb
- * Partner:   None
  * Date:      11/3/2020
  * Course:    CS 4540, University of Utah, School of Computing
- * Copyright: CS 4540 and Ro Erb - This work may not be copied for use in Academic Coursework.
- *
- * I, Ro Erb, certify that I wrote this code from scratch and did 
- * not copy it in part or whole from another source.  Any references used 
- * in the completion of the assignment are cited in my README file and in
- * the appropriate method header.
+ * Copyright: CS 4540 - This work may not be copied for use in Academic Coursework.
  *
  * File Contents
  *
